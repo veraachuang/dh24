9db91525186fd898cffdf07ac935aa7d359d6016
Angela Wu <angelawu097@gmail.com>
2024-10-12 20:13:54 -0700

    added notification handler to process disconnect/connects (client)

--- client/App.js
+++ client/App.js
@@ -1,22 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, SafeAreaView, ScrollView } from 'react-native';
 import SomeComponent from './src/SomeComponent';
+import React, { useState, useEffect } from 'react';
+import NotificationClient from "./NotificationHandler";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is me testing how to use Expo Go!</Text>
-      <StatusBar style="auto" />
-      <SomeComponent />
-    </View>
+
+    <SafeAreaView style={styles.container}>
+        <NotificationClient />
+        <ScrollView style = {styles.scrollView}>
+            <Text style = {styles.text}>Have fun on your backpacking trip!</Text>
+            <StatusBar style="auto" />
+        </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+    },
+    scrollView: {
+        backgroundColor: 'light-blue',
+        marginHorizontal: 20,
+    },
+    text: {
+        fontSize: 42,
+        align: "center",
+        font: 'Roboto',
+        justifyContent: "center"
+    },
 });
+
+
+
