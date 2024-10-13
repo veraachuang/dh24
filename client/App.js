b9c2056164a92941bdf9ef9782645c14dc835bd6
vera chuang <verachuanghh0809@gmail.com>
2024-10-12 22:48:19 -0700

    vera's branch is now main branch?

--- client/App.js
+++ client/App.js
@@ -1,22 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SomeComponent from './src/SomeComponent';
+import { StyleSheet, Text, View, Alert, SafeAreaView, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import LocationStatusHandler from './src/LocationHandler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is me testing how to use Expo Go!</Text>
-      <StatusBar style="auto" />
-      <SomeComponent />
-    </View>
+
+    <SafeAreaView style={styles.container}>
+        <Text style = {styles.h1}> S  P  O  T  T  Y</Text>
+        <ScrollView style = {styles.scrollView}>
+            <Text style = {styles.text}>Have fun on your backpacking trip!</Text>
+             <StatusBar style="auto" />
+            <LocationStatusHandler/>
+        </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+    },
+    scrollView: {
+        backgroundColor: 'light-blue',
+        marginHorizontal: 20,
+    },
+    text: {
+        fontSize: 20,
+        textAlignVertical: "center",
+        font: 'Roboto',
+        justifyContent: "center"
+    },
+    h1: {
+        fontSize: 40,
+        align: "center",
+        font: 'Roboto',
+        justifyContent: "center",
+        textAlign: "center",
+    }
 });
--- server/src/routes.js
+++ server/src/routes.js
@@ -17,14 +17,13 @@ export async function subscribeToNokiaService() {
     // Create a connectivity subscription with an expiry date-time stamp
     const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
 
-// Simply change the event_type to
-// "org.camaraproject.device-status.v0.roaming-status" whenever needed.
+    // Simply change the event_type to
+    // "org.camaraproject.device-status.v0.roaming-status" whenever needed.
     const mySubscription = await client.deviceStatus.subscribe(
         myDevice,
         "org.camaraproject.device-status.v0.connectivity-data",
         // Use HTTPS to send notifications
-        "https://localhost:8088.com/notifyConnect",
-        {
+        "https://localhost:8088.com/notifyConnect", {
             notificationAuthToken: "<auth-token>",
             // Pass the previously created date-time object
             subscriptionExpireTime: expirationDate
@@ -35,8 +34,7 @@ export async function subscribeToNokiaService() {
         myDevice,
         "org.camaraproject.device-status.v0.connectivity-disconnected",
         // Use HTTPS to send notifications
-        "https://localhost:8088.com/notifyDisconnect",
-        {
+        "https://localhost:8088.com/notifyDisconnect", {
             notificationAuthToken: "<auth-token>",
             // Pass the previously created date-time object
             subscriptionExpireTime: expirationDate
@@ -45,7 +43,7 @@ export async function subscribeToNokiaService() {
 
 
 
-// Get the subscription previously created by its ID
+    // Get the subscription previously created by its ID
     const subscription = await client.deviceStatus.get(
         mySubscription.eventSubscriptionId
     );
@@ -53,13 +51,12 @@ export async function subscribeToNokiaService() {
         mySubscription2.eventSubscriptionId
     );
 
-// Retrieve list of active Device Status subscriptions for a client:
+    // Retrieve list of active Device Status subscriptions for a client:
     const subscriptions = await client.deviceStatus.getSubscriptions();
 
-// Check the connectivity or roaming status
-// subscriptions of a given device
+    // Check the connectivity or roaming status
+    // subscriptions of a given device
     const connectivityStatus = await myDevice.getConnectivity();
 
     const roamingStatus = await myDevice.getRoaming();
 }
-
