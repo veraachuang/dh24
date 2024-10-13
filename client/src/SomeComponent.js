f888d776a26bd1878a14e7e449fbaecbcac02ac0
vera chuang <verachuanghh0809@gmail.com>
2024-10-12 22:48:19 -0700

    added location retrieval and verification

--- client/src/SomeComponent.js
+++ client/src/SomeComponent.js
@@ -2,20 +2,18 @@ import { Text, StyleSheet } from "react-native"; // Here we are using the React 
 
 // By using export, you can import and use this component in your app!
 export default SomeComponent = () => {
-  let number = 100;
+    let number = 100;
 
-  // This is what we want the component to return.
-  return (
-    <Text style={styles.text}> {/* Here I am using the styles StyleSheet to change the text's color. */}
-      This is a component.
-      I can display data, {number}, using curly brackets!
+    // This is what we want the component to return.
+    return (< Text style={styles.text} > { /* Here I am using the styles StyleSheet to change the text's color. */}
+        This is a component.I can display data, {number}, using curly brackets!
     </Text>
-  )
+    )
 }
 
 // You can style components and tags using StyleSheet
 const styles = StyleSheet.create({
-  text: {
-    color: 'red',
-  },
+    text: {
+        color: 'red',
+    },
 });
